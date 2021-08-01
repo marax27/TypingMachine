3612f3569f706b9342d4f0c33252182b31ff2bf5
marax27 <kacpert314@gmail.com>
2021-08-01 17:59:15 +0200

    Create NamespaceEntity

---
+++ src/TypingMachine/Entities/NamespaceEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TypingMachine.Entities
+{
+    public class NamespaceEntity : IEquatable<NamespaceEntity>
+    {
+        public ICollection<string> Sections { get; }
+
+        public string GetFullName()
+        {
+            var result = string.Join(".", Sections);
+            return string.IsNullOrEmpty(result) ? "<root>" : result;
+        }
+
+        private NamespaceEntity(ICollection<string> sections)
+        {
+            Sections = sections;
+        }
+
+        public static NamespaceEntity Create(ICollection<string> sections)
+        {
+            if (sections == null)
+                throw new ArgumentNullException(nameof(sections));
+            if (sections.Count == 0)
+                throw new ArgumentOutOfRangeException(nameof(sections), $"No sections provided. To represent root-level namespace, use {nameof(NoNamespace)}.");
+            return new NamespaceEntity(sections);
+        }
+
+        public static NamespaceEntity NoNamespace
+            => new(new List<string>());
+
+        public bool Equals(NamespaceEntity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Sections.SequenceEqual(other.Sections);
+        }
+
+        public override bool Equals(object obj) => Equals(obj as NamespaceEntity);
+        public override int GetHashCode() => GetFullName().GetHashCode();
+        public static bool operator ==(NamespaceEntity left, NamespaceEntity right) => Equals(left, right);
+        public static bool operator !=(NamespaceEntity left, NamespaceEntity right) => !Equals(left, right);
+    }
+}
---
+++ tests/TypingMachine.Tests/NamespaceEntityTests/WhenCreatingNamespaceEntity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using TypingMachine.Entities;
+using Xunit;
+
+namespace TypingMachine.Tests.NamespaceEntityTests
+{
+    public class WhenCreatingNamespaceEntity
+    {
+        private string[] GivenSections => new[] {"ProjectName", "Application", "Services"};
+
+        [Fact]
+        public void GivenValidSections_CreateEntity()
+        {
+            Action act = () =>
+            {
+                var entity = NamespaceEntity.Create(GivenSections);
+            };
+
+            act.Should().NotThrow();
+        }
+
+        [Fact]
+        public void GivenValidSections_ContainExpectedNumberOfSections()
+        {
+            var entity = NamespaceEntity.Create(GivenSections);
+
+            entity.Sections.Should().HaveCount(3);
+        }
+
+        [Fact]
+        public void GivenNullSections_ThrowExpectedException()
+        {
+            Action act = () =>
+            {
+                var entity = NamespaceEntity.Create(null!);
+            };
+
+            act.Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be("sections");
+        }
+
+        [Fact]
+        public void GivenZeroSections_ThrowExceptionWithExpectedParameterName()
+        {
+            Action act = () =>
+            {
+                var entity = NamespaceEntity.Create(new List<string>());
+            };
+
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .Which.ParamName.Should().Be("sections");
+        }
+
+        [Fact]
+        public void GivenZeroSections_ThrowExceptionWithExpectedMessage()
+        {
+            Action act = () =>
+            {
+                var entity = NamespaceEntity.Create(new List<string>());
+            };
+
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .Which.Message.Should().Contain("No sections provided. To represent root-level namespace, use NoNamespace.");
+        }
+    }
+}
