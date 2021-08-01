37a011f80449193fc095e0d69dc4f993d047e1ab
marax27 <kacpert314@gmail.com>
2021-08-01 22:52:21 +0200

    Create FieldEntity

---
+++ src/TypingMachine/Entities/FieldEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypingMachine.Entities
+{
+    public class FieldEntity
+    {
+        public string Name { get; }
+        public TypeIdentifier Type { get; }
+
+        private FieldEntity(string name, TypeIdentifier type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public static FieldEntity Create(string name, TypeIdentifier type)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("String is empty or whitespace-only.", nameof(name));
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            return new FieldEntity(name, type);
+        }
+    }
+}
