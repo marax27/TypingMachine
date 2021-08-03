f021b5813b37e615fd1edbb40929edb2afa45136
marax27 <kacpert314@gmail.com>
2021-08-03 22:59:43 +0200

    Create UsingEntity and UsingWalker

---
+++ src/TypingMachine/CodeWalkers/UsingWalker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TypingMachine.Entities;
+
+namespace TypingMachine.CodeWalkers
+{
+    /// <summary>
+    /// Finds all `using` directives in provided syntax tree.
+    /// </summary>
+    public class UsingWalker : CSharpSyntaxWalker
+    {
+        private ICollection<UsingEntity> _usingEntities;
+
+        public IReadOnlyCollection<UsingEntity> FindAll(SyntaxNode rootNode)
+        {
+            _usingEntities = new List<UsingEntity>();
+            Visit(rootNode);
+            return _usingEntities.ToList();
+        }
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            base.VisitUsingDirective(node);
+
+            var ns = NamespaceEntity.Create(node.Name.ToString().Split('.'));
+            _usingEntities.Add(UsingEntity.Create(ns));
+        }
+    }
+}
---
+++ tests/TypingMachine.Tests/CodeWalking/UsingWalkerTests/DiscoveringUsingEntitiesTestContexts.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using TypingMachine.Entities;
+
+namespace TypingMachine.Tests.CodeWalking.UsingWalkerTests
+{
+    public interface IDiscoveringUsingEntitiesTestContext
+    {
+        string GivenSource { get; }
+        IReadOnlyCollection<UsingEntity> ExpectedResult { get; }
+    }
+
+    class ZeroUsingDirectivesContext : IDiscoveringUsingEntitiesTestContext
+    {
+        public string GivenSource => @"
+namespace Application.Controllers
+{
+    class HelloController {}
+}
+";
+
+        public IReadOnlyCollection<UsingEntity> ExpectedResult
+            => new List<UsingEntity>();
+    }
+
+    class OneUsingDirectiveContext : IDiscoveringUsingEntitiesTestContext
+    {
+        public string GivenSource => @"
+using System.Data;
+
+namespace Application.Controllers
+{
+    class HelloController {}
+}
+";
+
+        public IReadOnlyCollection<UsingEntity> ExpectedResult
+            => new List<UsingEntity>
+            {
+                UsingEntity.Create(NamespaceEntity.Create(new []{"System", "Data"}))
+            };
+    }
+
+    class ThreeUsingDirectivesContext : IDiscoveringUsingEntitiesTestContext
+    {
+        public string GivenSource => @"
+using System;
+using System.Collections.Generic;
+
+using Application.Utilities;
+
+namespace Application.Controllers
+{
+    class HelloController {}
+}
+";
+
+        public IReadOnlyCollection<UsingEntity> ExpectedResult
+            => new List<UsingEntity>
+            {
+                UsingEntity.Create(NamespaceEntity.Create(new []{"System"})),
+                UsingEntity.Create(NamespaceEntity.Create(new []{"System", "Collections", "Generic"})),
+                UsingEntity.Create(NamespaceEntity.Create(new []{"Application", "Utilities"}))
+            };
+    }
+}
